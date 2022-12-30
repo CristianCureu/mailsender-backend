f261b5efc5050e8591b597397f2be384cc3a32c2
CristianCureu <ccristi_01@yahoo.com>
2022-12-30 18:07:58 +0200

    send mail

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
-
 const app = express();
+
+const mail = require("./mail");
+
+const sendPromoMail = async () => {
+  try {
+    await mail.sendMail("cureu.ncristian@gmail.com", "Welcome", "promo", {
+      name: "Cristian",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// sendPromoMail();
 
 app.listen(5000, () => {
   console.log("Server up!");
---
+++ mail.js
@@ -0,0 +1,33 @@
+const sgMail = require("@sendgrid/mail");
+const Handlebars = require("handlebars");
+const config = require("./config");
+const fs = require("fs");
+const path = require("path");
+
+const sendMail = (recipient, subject, templateName, data) => {
+  const emailTemplate = fs.readFileSync(
+    path.join(__dirname, `/Templates/${templateName}.handlebars`),
+    "utf-8"
+  );
+  return new Promise((resolve, reject) => {
+    if (config.SENDGRID_API_KEY) {
+      sgMail.setApiKey(config.SENDGRID_API_KEY);
+      const template = Handlebars.compile(emailTemplate);
+      const messageBody = template(data);
+
+      const msg = {
+        to: recipient,
+        from: config.SENDGRID_EMAIL_SENDER,
+        subject,
+        text: "This message is sent automatically",
+        html: messageBody,
+      };
+
+      sgMail.send(msg).then(resolve).catch(reject);
+    } else {
+      reject(new Error("Sendgrid API key missing"));
+    }
+  });
+};
+
+module.exports = { sendMail };
